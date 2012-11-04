4225f9918505f406c9f05a8a2a681e2377727744
Deion <deionog@gmail.com>
2012-11-03 22:25:57 -0400

    Update query on zoom

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
+var map,layer, layer_2,
+    state_layer,
+    public_layer,
+    private_layer;
+var year = '2010-11';
+
+
 function initialize() {
 
-    var unitedstates = new google.maps.LatLng(40.4230, -98.7372);
-
-    var map = new google.maps.Map(document.getElementById('map_canvas'), {
-        center   :unitedstates,
+    map = new google.maps.Map(document.getElementById('map_canvas'), {
+        center   :new google.maps.LatLng(40.4230, -98.7372),
         zoom     :4,
         mapTypeId:google.maps.MapTypeId.ROADMAP
     });
 
-    var layer = new google.maps.FusionTablesLayer({
+    // Initialize State Level Layer
+    //get_layer('state',null);
+    layer = new google.maps.FusionTablesLayer({
         query: {
             select: 'geometry',
             from: '1b-RZu6Cu4xDud8JEKCNEyzPXnnQ0suTk2KGs_Mk'
         }
     });
-
     layer.setMap(map);
+
+    // Initialize Empty Second Layer to contain Private Schools later
+    layer_2 = new google.maps.FusionTablesLayer({
+
+    });
+
+    layer_2.setMap(map);
 
     google.maps.event.addListener(map, 'zoom_changed', function(){
 
@@ -24,30 +37,93 @@ function initialize() {
 
         // When zoom is finer than 6 get institutions
         if (zoomLevel >= statezoom) {
-            layer.setMap(null);
-            layer = new google.maps.FusionTablesLayer({
+
+            // Add Public Institutions to layer
+            layer.setOptions({
                 query: {
-                    select: 'Longitude location of institution(HD2011)',
-                    from: '1Y0LFXEJRk0r0NIqZYEu66_0AHDelx0wClEJLW5w'
+                    select: 'longitude',
+                    from: '1RMj6bytivb9T1EvGcGWHrPw8rWv8Ze3UVdIOZUE',
+                    where: "Year = " + year
+                }
+            });
+
+            // Add Private Institutions  to layer 2
+            layer_2.setOptions({
+                query: {
+                    select: 'longitude',
+                    from: '16k0RPYOl7XRRaXOEL710C1HDXW3q6cfSk01gInY',
+                    where: "Year = " + year
                 }
             });
 
 
-            layer.setMap(map);
-        } else {
+        } else if (zoomLevel < statezoom){
 
-            layer.setMap(null);
-            layer = new google.maps.FusionTablesLayer({
+            // Re-Display State Layer AND Remove Public Institution Layer
+            layer.setOptions({
                 query: {
                     select: 'geometry',
                     from: '1b-RZu6Cu4xDud8JEKCNEyzPXnnQ0suTk2KGs_Mk'
                 }
             });
 
-            layer.setMap(map);
+            // Removes Private Institutions layer
+            layer_2.setOptions({
+                query: null
+
+            });
+
         }
     });
 }
+
+
+// Returns a Map Layer Based on the Type[public / private] and Year - DEPRECATED!!!..... smh
+function get_layer(type,year){
+    //var layer = null;
+
+    switch (type){
+        case 'public': {
+            layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select: 'longitude',
+                    from: '1RMj6bytivb9T1EvGcGWHrPw8rWv8Ze3UVdIOZUE',
+                    where: "Year = " + year
+                }
+
+            });
+            layer.setMap(map);
+            break;
+        }
+        case 'private': {
+            layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select: 'longitude',
+                    from: '16k0RPYOl7XRRaXOEL710C1HDXW3q6cfSk01gInY',
+                    where: "Year = " + year
+                }
+
+            });
+            layer.setMap(map);
+            break;
+        }
+        case 'state' : {
+            layer = new google.maps.FusionTablesLayer({
+                query: {
+                    select: 'geometry',
+                    from: '1b-RZu6Cu4xDud8JEKCNEyzPXnnQ0suTk2KGs_Mk'
+                }
+            });
+            layer.setMap(map);
+            break;
+        }
+
+
+    }
+
+    //return layer;
+
+} // End get_layer
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
